abe989a8518242a9e180254c6ca867a950423a45
keke78ui9 <keke78ui9@gmail.com>
2015-10-07 23:45:41 -0700

    sample code to call MongoDB 3.0.5 by C# driver

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,50 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                AccessMongo();
+            }
+            catch (Exception ex)
+            {
+                // any error
+            }
+        }
+
+        /// <summary>
+        /// mongo server version: db.version() 3.0.5
+        /// </summary>
+        private static void AccessMongo()
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+
+            var database = client.GetDatabase("Test");
+
+            var collection = database.GetCollection<BsonDocument>("SampleData");
+
+            var documents = collection.Find(new BsonDocument()).ToListAsync().Result;
+
+            var document = new BsonDocument
+            {
+                { "name", Guid.NewGuid().ToString() },
+                { "age", documents.Count() + 1 }
+            };
+
+            collection.InsertOneAsync(document);
+
+
+
+        }
+    }
+}
